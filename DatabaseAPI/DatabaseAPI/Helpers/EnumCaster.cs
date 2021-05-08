5bf59240fde0935ec971709badd51b1a3c91a263
Dzemoro <dellorpl@gmail.com>
2021-05-08 18:44:09 +0200

    All switches

--- DatabaseAPI/DatabaseAPI/Helpers/EnumCaster.cs
+++ DatabaseAPI/DatabaseAPI/Helpers/EnumCaster.cs
@@ -41,5 +41,91 @@ namespace DatabaseAPI.Helpers
                     throw new Exception();
             }
         }
+
+        public static UserType UserTypeFromString(string type)
+        {
+            switch (type)
+            {
+                case "Admin":
+                    return UserType.Admin;
+                case "Manager":
+                    return UserType.Manager;
+                default:
+                    throw new ArgumentException("Type should be Admin or Manager");
+            }
+        }
+
+        public static string UserTypeToString(UserType type)
+        {
+            switch (type)
+            {
+                case UserType.Admin:
+                    return "Admin";
+                case UserType.Manager:
+                    return "Manager";
+                default:
+                    throw new Exception();
+            }
+        }
+
+        public static ProductStatus ProductStatusFromString(string status)
+        {
+            switch (status)
+            {
+                case "Available":
+                    return ProductStatus.Available;
+                case "Withdrawn":
+                    return ProductStatus.Withdrawn;
+                case "Paused":
+                    return ProductStatus.Paused;
+                default:
+                    throw new ArgumentException("Status should be one of Available, Withdrawn, Paused");
+            }
+        }
+
+        public static string ProductStatusToString(ProductStatus status)
+        {
+            switch (status)
+            {
+                case ProductStatus.Available:
+                    return "Available";
+                case ProductStatus.Withdrawn:
+                    return "Withdrawn";
+                case ProductStatus.Paused:
+                    return "Paused";
+                default:
+                    throw new Exception();
+            }
+        }
+
+        public static DiscountType DiscountTypeFromString(string type)
+        {
+            switch (type)
+            {
+                case "ItemsSet":
+                    return DiscountType.ItemsSet;
+                case "PriceDrop":
+                    return DiscountType.PriceDrop;
+                case "PercentagePriceDrop":
+                    return DiscountType.PercentagePriceDrop;
+                default:
+                    throw new ArgumentException("Type should be one of ItemsSet, PriceDrop, PercentagePriceDrop");
+            }
+        }
+
+        public static string DiscountTypeToString(DiscountType type)
+        {
+            switch (type)
+            {
+                case DiscountType.ItemsSet:
+                    return "ItemsSet";
+                case DiscountType.PriceDrop:
+                    return "PriceDrop";
+                case DiscountType.PercentagePriceDrop:
+                    return "PercentagePriceDrop";
+                default:
+                    throw new Exception();
+            }
+        }
     }
 }
