9f4bf9521ca95f39d9c0c41d69ed6b7d905415c1
ElStempel <steplewski.aleks@gmail.com>
2021-05-23 21:07:08 +0200

    kitchen gui work in progress

--- DatabaseAPI/KitchenApp/Form1.Designer.cs
+++ DatabaseAPI/KitchenApp/Form1.Designer.cs
@@ -29,13 +29,65 @@ namespace KitchenApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 209);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Numer 1\r\n---\r\nProdukt 1\r\nProdukt 2\r\n- komentarz";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(261, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 209);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Numer 2\r\n---\r\nProdukt 3 x2\r\nProdukt 4";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(521, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 209);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Numer 3\r\n---\r\nProdukt 1\r\nProdukt 5 x2";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(969, 585);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Kitchen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- DatabaseAPI/KitchenApp/Form1.cs
+++ DatabaseAPI/KitchenApp/Form1.cs
@@ -16,5 +16,10 @@ namespace KitchenApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
