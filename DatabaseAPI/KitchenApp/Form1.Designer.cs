5ca50d35edf1e9b197e39c634ee84efc00558027
ElStempel <steplewski.aleks@gmail.com>
2021-05-24 15:34:08 +0200

    gui setup, api next

--- DatabaseAPI/KitchenApp/Form1.Designer.cs
+++ DatabaseAPI/KitchenApp/Form1.Designer.cs
@@ -29,62 +29,125 @@ namespace KitchenApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.listBox1);
+            this.flowLayoutPanel1.Controls.Add(this.listBox2);
+            this.flowLayoutPanel1.Controls.Add(this.listBox3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(835, 363);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Zamówienie 1",
+            "---",
+            "Produkt 1",
+            "Produkt 2 x2",
+            "Produkt 4"});
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(266, 324);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "Zamówienie 2",
+            "---",
+            "Produkt 3",
+            "-komentarz"});
+            this.listBox2.Location = new System.Drawing.Point(275, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(270, 324);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Items.AddRange(new object[] {
+            "Zamówienie 3",
+            "---",
+            "Produkt 1  x3",
+            "Produkt 2"});
+            this.listBox3.Location = new System.Drawing.Point(551, 3);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(259, 324);
+            this.listBox3.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(61, 429);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 209);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Numer 1\r\n---\r\nProdukt 1\r\nProdukt 2\r\n- komentarz";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Size = new System.Drawing.Size(175, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Wykonano";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(261, 12);
+            this.button2.Location = new System.Drawing.Point(345, 429);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(222, 209);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Numer 2\r\n---\r\nProdukt 3 x2\r\nProdukt 4";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Size = new System.Drawing.Size(175, 66);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Wykonano";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(521, 12);
+            this.button3.Location = new System.Drawing.Point(612, 429);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(226, 209);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Numer 3\r\n---\r\nProdukt 1\r\nProdukt 5 x2";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.Size = new System.Drawing.Size(175, 66);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Wykonano";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(969, 585);
+            this.ClientSize = new System.Drawing.Size(874, 585);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Kitchen";
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- DatabaseAPI/KitchenApp/Form1.cs
+++ DatabaseAPI/KitchenApp/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using RestSharp;
+using RestSharp.Authenticators;
+
 namespace KitchenApp
 {
     public partial class Form1 : Form
@@ -15,11 +18,27 @@ namespace KitchenApp
         public Form1()
         {
             InitializeComponent();
+            //var client = new RestClient("https://api.twitter.com/1.1");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Wykonano zamówienie, wczytuję nowe");
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox2.Items.Add("Wykonano zamówienie, wczytuję nowe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            listBox3.Items.Add("Wykonano zamówienie, wczytuję nowe");
         }
     }
 }
