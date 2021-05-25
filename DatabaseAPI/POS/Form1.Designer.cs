85d8d8318c293a3c554f5da3f08ad2c7ce339f6e
konigin144 <kinga777s@gmail.com>
2021-05-25 21:30:34 +0200

    Order panel - adding and removing products

---
+++ DatabaseAPI/POS/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace POSApp {
+    partial class Form1 {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Produkt1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Produkt2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Kategoria1", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Produkt1");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Produkt2");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Produkt3");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Kategoria2", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Produkt1");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Produkt2");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Kategoria3", new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode9});
+            this.ProductsTreeView = new System.Windows.Forms.TreeView();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.PayButton = new System.Windows.Forms.Button();
+            this.OrderTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Test = new System.Windows.Forms.Label();
+            this.SumTextBox = new System.Windows.Forms.TextBox();
+            this.OrderTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProductsTreeView
+            // 
+            this.ProductsTreeView.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ProductsTreeView.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ProductsTreeView.Location = new System.Drawing.Point(12, 12);
+            this.ProductsTreeView.Name = "ProductsTreeView";
+            treeNode1.Name = "Węzeł3";
+            treeNode1.Text = "Produkt1";
+            treeNode2.Name = "Węzeł4";
+            treeNode2.Text = "Produkt2";
+            treeNode3.Name = "Węzeł0";
+            treeNode3.Text = "Kategoria1";
+            treeNode4.Name = "Węzeł5";
+            treeNode4.Text = "Produkt1";
+            treeNode5.Name = "Węzeł6";
+            treeNode5.Text = "Produkt2";
+            treeNode6.Name = "Węzeł7";
+            treeNode6.Text = "Produkt3";
+            treeNode7.Name = "Węzeł1";
+            treeNode7.Text = "Kategoria2";
+            treeNode8.Name = "Węzeł8";
+            treeNode8.Text = "Produkt1";
+            treeNode9.Name = "Węzeł9";
+            treeNode9.Text = "Produkt2";
+            treeNode10.Name = "Węzeł2";
+            treeNode10.Text = "Kategoria3";
+            this.ProductsTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode7,
+            treeNode10});
+            this.ProductsTreeView.Size = new System.Drawing.Size(220, 426);
+            this.ProductsTreeView.TabIndex = 0;
+            this.ProductsTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.ProductsTreeView_NodeMouseDoubleClick);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.Color.IndianRed;
+            this.CancelButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CancelButton.Location = new System.Drawing.Point(708, 358);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(80, 80);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Anuluj";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // PayButton
+            // 
+            this.PayButton.BackColor = System.Drawing.Color.LightGreen;
+            this.PayButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PayButton.Location = new System.Drawing.Point(575, 358);
+            this.PayButton.Name = "PayButton";
+            this.PayButton.Size = new System.Drawing.Size(127, 80);
+            this.PayButton.TabIndex = 2;
+            this.PayButton.Text = "Zapłać";
+            this.PayButton.UseVisualStyleBackColor = false;
+            this.PayButton.Click += new System.EventHandler(this.PayButton_Click);
+            // 
+            // OrderTableLayoutPanel
+            // 
+            this.OrderTableLayoutPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.OrderTableLayoutPanel.ColumnCount = 5;
+            this.OrderTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.OrderTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.OrderTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.OrderTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.OrderTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.OrderTableLayoutPanel.Controls.Add(this.label1, 0, 0);
+            this.OrderTableLayoutPanel.Controls.Add(this.label5, 1, 0);
+            this.OrderTableLayoutPanel.Controls.Add(this.label6, 2, 0);
+            this.OrderTableLayoutPanel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.OrderTableLayoutPanel.Location = new System.Drawing.Point(238, 12);
+            this.OrderTableLayoutPanel.Name = "OrderTableLayoutPanel";
+            this.OrderTableLayoutPanel.RowCount = 1;
+            this.OrderTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.OrderTableLayoutPanel.Size = new System.Drawing.Size(550, 340);
+            this.OrderTableLayoutPanel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Produkt";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 22);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Liczba";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(333, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cena";
+            // 
+            // Test
+            // 
+            this.Test.AutoSize = true;
+            this.Test.Location = new System.Drawing.Point(771, 428);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(28, 13);
+            this.Test.TabIndex = 4;
+            this.Test.Text = "Test";
+            // 
+            // SumTextBox
+            // 
+            this.SumTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.SumTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SumTextBox.Location = new System.Drawing.Point(238, 358);
+            this.SumTextBox.Name = "SumTextBox";
+            this.SumTextBox.ReadOnly = true;
+            this.SumTextBox.Size = new System.Drawing.Size(331, 80);
+            this.SumTextBox.TabIndex = 5;
+            this.SumTextBox.Text = "32,64 zł";
+            this.SumTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SumTextBox);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.OrderTableLayoutPanel);
+            this.Controls.Add(this.PayButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.ProductsTreeView);
+            this.Name = "Form1";
+            this.Text = "POS";
+            this.OrderTableLayoutPanel.ResumeLayout(false);
+            this.OrderTableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView ProductsTreeView;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button PayButton;
+        private System.Windows.Forms.TableLayoutPanel OrderTableLayoutPanel;
+        private System.Windows.Forms.Label Test;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox SumTextBox;
+    }
+}
+
---
+++ DatabaseAPI/POS/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSApp {
+
+
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void PayButton_Click(object sender, EventArgs e) {
+            // todo
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e) {
+            // todo
+        }
+
+        private void updateSumTextBox() {
+            // todo
+        }
+
+        private void ProductsTreeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e) {
+            if (sender is TreeView) {
+                if (((TreeView)sender).SelectedNode.LastNode == null) {
+                    Button delButton = new DeleteButtonClass().DeleteButton;
+                    delButton.Click += new System.EventHandler(this.DeleteButton_Click);
+
+                    this.OrderTableLayoutPanel.RowCount += 1;
+                    this.OrderTableLayoutPanel.Controls.Add(new Label() { Font = new Font("Century Gothic", 14), Text = ((TreeView)sender).SelectedNode.Text }, 0, this.OrderTableLayoutPanel.RowCount - 1);
+                    this.OrderTableLayoutPanel.Controls.Add(new Label() { Font = new Font("Century Gothic", 14), Text = "5" }, 1, this.OrderTableLayoutPanel.RowCount - 1);
+                    this.OrderTableLayoutPanel.Controls.Add(new Label() { Font = new Font("Century Gothic", 14), Text = "7" }, 2, this.OrderTableLayoutPanel.RowCount - 1);
+                    this.OrderTableLayoutPanel.Controls.Add(new Button() { Font = new Font("Century Gothic", 9), Text = "e" }, 3, this.OrderTableLayoutPanel.RowCount - 1);
+                    this.OrderTableLayoutPanel.Controls.Add(delButton, 4, this.OrderTableLayoutPanel.RowCount - 1);
+
+                    //updateSumTextBox();
+                }
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e) {
+            var panel = OrderTableLayoutPanel;
+            int rowIndex = OrderTableLayoutPanel.GetPositionFromControl((Button)sender).Row;
+
+            // delete all controls of row that we want to delete
+            for (int i = 0; i < panel.ColumnCount; i++) {
+                var control = panel.GetControlFromPosition(i, rowIndex);
+                panel.Controls.Remove(control);
+            }
+
+            // move up row controls that comes after row we want to remove
+            for (int i = rowIndex + 1; i < panel.RowCount; i++) {
+                for (int j = 0; j < panel.ColumnCount; j++) {
+                    var control = panel.GetControlFromPosition(j, i);
+                    if (control != null) {
+                        panel.SetRow(control, i - 1);
+                    }
+                }
+            }
+
+            var removeStyle = panel.RowCount - 1;
+
+            if (panel.RowStyles.Count > removeStyle)
+                panel.RowStyles.RemoveAt(removeStyle);
+
+            panel.RowCount--;
+
+        }
+
+    }
+
+    class DeleteButtonClass {
+        public Button DeleteButton;
+        public DeleteButtonClass() {
+            this.DeleteButton = new Button();
+            this.DeleteButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.TabIndex = 14;
+            this.DeleteButton.Text = "d";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+        }
+    }
+}
+
+// todo:
+// quantity of products
+// fill up the left products panel
+// is edit button necessary?
