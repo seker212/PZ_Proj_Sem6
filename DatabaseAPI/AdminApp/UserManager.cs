141c1fb3c4657ebc7a846e9bb113cfeab15937e6
Dzemoro <dellorpl@gmail.com>
2021-06-07 17:05:20 +0200

    Initial commit

---
+++ DatabaseAPI/AdminApp/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApp
+{
+    public class UserManager
+    {
+        User User { get; set; }
+
+        public UserManager(User user)
+        {
+            User = user;
+        }
+
+        public bool VerifyUser()
+        {
+            //TODO: Write body
+            return true;
+        }
+
+        public void GetUserType()
+        {
+            //TODO: Write body
+        }
+ 
+    }
+}
