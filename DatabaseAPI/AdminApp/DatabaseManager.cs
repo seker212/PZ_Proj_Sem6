8e29a41bdf1f837024047eb14c276e020455e1cb
Dzemoro <dellorpl@gmail.com>
2021-06-13 23:15:48 +0200

    Commit before changes from api

--- DatabaseAPI/AdminApp/DatabaseManager.cs
+++ DatabaseAPI/AdminApp/DatabaseManager.cs
@@ -37,8 +37,14 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
@@ -53,8 +59,14 @@ namespace AdminApp
                 var cashier = JsonConvert.DeserializeObject<Cashier>(response.Content);
                 return cashier;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return null;
             }
         }
@@ -69,8 +81,14 @@ namespace AdminApp
                 var cashier = JsonConvert.DeserializeObject<List<Cashier>>(response.Content);
                 return cashier;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return null;
             }
         }
@@ -95,8 +113,14 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
@@ -118,13 +142,19 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
 
-        public bool AddDiscount(bool isAvailable, double setPrice, double priceDropAmount, double priceDropPercent)
+        public bool AddDiscount(bool isAvailable, double? setPrice, double? priceDropAmount, double? priceDropPercent)
         {
             var discount = new Discount
             {
@@ -134,7 +164,7 @@ namespace AdminApp
                 PriceDropAmount = priceDropAmount,
                 PriceDropPercent = priceDropPercent
             };
-            var request = new RestRequest("api/crud/cashier");
+            var request = new RestRequest("api/crud/Discount");
             string jsonBody = JsonConvert.SerializeObject(discount);
             request.AddHeader("sessionId", SessionId);
             request.AddParameter("application/json; charset=utf-8", jsonBody, ParameterType.RequestBody);
@@ -143,8 +173,14 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
@@ -159,8 +195,14 @@ namespace AdminApp
                 var discount = JsonConvert.DeserializeObject<Discount>(response.Content);
                 return discount;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return null;
             }
         }
@@ -175,8 +217,14 @@ namespace AdminApp
                 var discount = JsonConvert.DeserializeObject<List<Discount>>(response.Content);
                 return discount;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return null;
             }
         }
@@ -205,8 +253,14 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
@@ -230,12 +284,146 @@ namespace AdminApp
             {
                 return true;
             }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
             else
             {
+                Console.WriteLine("Błędne parametry");
                 return false;
             }
         }
 
+        public bool AddDiscountSetItem(Guid discountId, Guid productId, int quantity)
+        {
+            var discountSetItem = new DiscountSetItem()
+            {
+                DiscountId = discountId,
+                ProductId = productId,
+                Quantity = quantity
+            };
+            var request = new RestRequest("api/crud/DiscountSetItem");
+            string jsonBody = JsonConvert.SerializeObject(discountSetItem);
+            request.AddHeader("sessionId", SessionId);
+            request.AddParameter("application/json; charset=utf-8", jsonBody, ParameterType.RequestBody);
+            var response = Client.Post(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Błędne parametry");
+                return false;
+            }
+        }
 
+        public DiscountSetItem GetDiscountSetItem(Guid guid)
+        {
+            //TODO: Repair guid in route
+            var request = new RestRequest("api/crud/DiscountSetItem/{guid}").AddUrlSegment("guid", guid);
+            request.AddHeader("sessionId", SessionId);
+            //request.AddParameter()
+            var response = Client.Get(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var discountSetItem = JsonConvert.DeserializeObject<DiscountSetItem>(response.Content);
+                return discountSetItem;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
+            else
+            {
+                Console.WriteLine("Błędne parametry");
+                return null;
+            }
+        }
+
+        public IEnumerable<DiscountSetItem> GetDiscountSetItems()
+        {
+            var request = new RestRequest("/api/crud/DiscountSetItem");
+            request.AddHeader("sessionId", SessionId);
+            var response = Client.Get(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var discountSetItem = JsonConvert.DeserializeObject<List<DiscountSetItem>>(response.Content);
+                return discountSetItem;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return null;
+            }
+            else
+            {
+                Console.WriteLine("Błędne parametry");
+                return null;
+            }
+        }
+
+        /*
+        public bool UpdateDiscountSetItem(Guid discountId, Guid productId, int quantity)
+        {
+            var discountSetItem = GetDiscountSetItem()
+            var request = new RestRequest("api/crud/Discount");
+            string jsonBody = JsonConvert.SerializeObject(discount);
+            request.AddHeader("sessionId", SessionId);
+            request.AddParameter("application/json; charset=utf-8", jsonBody, ParameterType.RequestBody);
+            var response = Client.Put(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Błędne parametry");
+                return false;
+            }
+        }
+        */
+
+        public bool DeleteDiscountSetItem(Guid discountId, Guid productId)
+        {
+            var discountSetItem = new DiscountSetItem()
+            {
+                DiscountId = discountId,
+                ProductId = productId,
+                Quantity = 1
+            };
+            string jsonBody = JsonConvert.SerializeObject(discountSetItem);
+            var request = new RestRequest("api/crud/DiscountSetItem");
+            request.AddHeader("sessionId", SessionId);
+            request.AddParameter("application/json; charset=utf-8", jsonBody, ParameterType.RequestBody);
+            var response = Client.Delete(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Console.WriteLine("Brak uprawnień");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Błędne parametry");
+                return false;
+            }
+        }
     }
 }
